18a1e42d3dd3f60e97182fce93deeaa9a3714b24
Momina Tufail <momnatufail30@gmail.com>
2025-01-02 23:39:39 +0500

    made widgets

---
+++ lib/Controller/Widgets/ResponsiveLayout.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+
+class ResponsiveLayout extends StatelessWidget {
+
+  Widget Desktop;
+  Widget Tablet;
+  Widget Mobile;
+
+  ResponsiveLayout({super.key,
+  required this.Desktop,
+  required this.Tablet,
+  required this.Mobile
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (context, constraints){
+          if (constraints.maxWidth<550) return Mobile;
+
+
+
+
+        });
+  }
+}
