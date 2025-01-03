bb6821c2b3f361e38764da16932449d81461efd8
Momina Tufail <momnatufail30@gmail.com>
2025-01-03 23:11:19 +0500

    code updated

--- lib/View/Home-View/Tablet-View/TabletHomeView.dart
+++ lib/View/Home-View/Tablet-View/TabletHomeView.dart
@@ -15,11 +15,11 @@ class _TabletHomeViewState extends State<TabletHomeView> {
         height: double.infinity,
         width: double.infinity,
         color: Colors.lightGreenAccent,
-        child: Center(child: Text('Tablet Viewssss',style: TextStyle(fontWeight:FontWeight.bold, color: Colors.black87,fontSize: 15),
+        child: Center(child: Text('Tablet Views',style: TextStyle(fontWeight:FontWeight.bold, color: Colors.black87,fontSize: 15),
 
 
     ),
       ),
-      ));
+
   }
 }
