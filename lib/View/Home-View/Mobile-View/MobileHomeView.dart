6aa9b22f7ef4f08fcacd5a60edbbba0bcd0abaa5
Momina Tufail <momnatufail30@gmail.com>
2025-01-03 23:30:54 +0500

    code updated

--- lib/View/Home-View/Mobile-View/MobileHomeView.dart
+++ lib/View/Home-View/Mobile-View/MobileHomeView.dart
@@ -15,7 +15,7 @@ class _MobileHomeViewState extends State<MobileHomeView> {
         height: double.infinity,
         width: double.infinity,
         color: Colors.purple.shade300,
-        child: Center(child: Text('Mobile View',style: TextStyle(fontWeight:FontWeight.bold,color: Colors.black87,fontSize: 20)),
+        child: Center(child: Text('Mobile View',style: TextStyle(fontWeight:FontWeight.bold,color: Colors.black87,fontSize: 19)),
 
     ),
 
